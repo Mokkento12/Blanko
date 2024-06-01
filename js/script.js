5a17cdaa45292d1bf5c1ca7cd8655ab801ca21d0
Roman Alekseev <roma.alekseev.90@bk.ru>
2024-06-01 19:25:43 +0300

    create: responsive footer, slider, landing

--- js/script.js
+++ js/script.js
@@ -6,11 +6,6 @@ const swiper = new Swiper(".swiper", {
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
+    clickable: true,
   },
-
-  // Navigation arrows
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
 });
