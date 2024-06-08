3e653c11251f2111ebd955216da73986013abd5f
Roman Alekseev <roma.alekseev.90@bk.ru>
2024-06-08 18:09:03 +0300

    create: close btn & fix

--- js/script.js
+++ js/script.js
@@ -11,12 +11,18 @@ const swiper = new Swiper(".swiper", {
 });
 
 const hamburger = document.querySelector(".hamburger");
+const closeBtn = document.querySelector(".close");
 const navLinks = document.querySelector(".nav__links");
 const headerInput = document.querySelector(".header__input");
 const headerButton = document.querySelector(".header__button");
+const headerContainer = document.querySelector(".header__container");
 
-hamburger.addEventListener("click", () => {
+function toggleMenu() {
   navLinks.classList.toggle("open");
   headerInput.classList.toggle("open");
   headerButton.classList.toggle("open");
-});
+  headerContainer.classList.toggle("menu-open");
+}
+
+hamburger.addEventListener("click", toggleMenu);
+closeBtn.addEventListener("click", toggleMenu);
